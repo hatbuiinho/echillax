6f7a68488f7210c2a62d4465ca4d016ddd7c3749
Huy Tuyen <fsoft.datnn@gmail.com>
2024-07-09 00:43:40 +0700

    update doctor advise

--- fe/app/_components/GetAdvise.tsx
+++ fe/app/_components/GetAdvise.tsx
@@ -1,20 +1,23 @@
-import React from "react";
-import GetAdviseForm from "./GetAdviseForm";
-import Image from "next/image";
-import { Spacer } from "@nextui-org/react";
 import DOCTOR from "@/assets/image/get-advise/doctor.png";
-import clsx from "clsx";
 import { fontBoosterBlack } from "@/config/fonts/fonts";
+import clsx from "clsx";
+import Image from "next/image";
+import GetAdviseForm from "./GetAdviseForm";
 
 const GetAdvise = () => {
   return (
     <div className="relative">
-      <div className="flex justify-center">
-        <div className="hidden w-[400px] items-end md:-m-10 md:flex md:h-[500px] md:w-[500px] lg:-m-20  lg:w-[700px]">
-          <Image className="h-auto w-full" src={DOCTOR} alt="" />
+      <div className="grid items-end justify-center md:grid-cols-2">
+        <div
+          className={clsx(
+            "relative left-10 hidden h-full  items-end justify-end md:flex",
+            "h-[400px] md:h-[500px] lg:h-[600px]",
+          )}
+        >
+          <Image className="h-full w-auto" src={DOCTOR} alt="" />
         </div>
-        <div className="get-advise-form flex min-h-96 flex-col">
-          <div className="text-md mb-3 flex justify-center  lg:justify-start ">
+        <div className="get-advise-form mb-20 flex min-h-96 flex-col">
+          <div className="text-md mb-3 flex  lg:justify-start ">
             <fieldset className="w-[300px] rounded-xl border-1 border-primary-500 uppercase text-primary lg:w-[400px] lg:justify-start lg:text-xl">
               <legend
                 className={clsx(
@@ -35,7 +38,9 @@ const GetAdvise = () => {
               </a>
             </fieldset>
           </div>
-          <GetAdviseForm />
+          <div className="w-[300px] lg:w-[400px]">
+            <GetAdviseForm />
+          </div>
         </div>
       </div>
     </div>
