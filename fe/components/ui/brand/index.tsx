0f9d3d44a15ce12112eb74c7bc6ee1ca8af9b278
Huy Tuyen <fsoft.datnn@gmail.com>
2024-07-11 10:41:46 +0700

    reload when click logo

--- fe/components/ui/brand/index.tsx
+++ fe/components/ui/brand/index.tsx
@@ -7,7 +7,7 @@ type Props = { className?: string };
 
 const Brand = ({ className }: Props) => {
   return (
-    <Link
+    <a
       className={clsx(
         "relative flex h-full flex-shrink-0 items-center rounded-b-lg bg-amber-200 px-2",
         "before::content-[''] before:absolute  before:-z-10 before:h-full before:w-full before:-translate-x-2 before:translate-y-[5px] before:scale-[98%] before:rounded-b-xl before:bg-primary before:px-2",
@@ -18,7 +18,7 @@ const Brand = ({ className }: Props) => {
       <div className="flex-shrink-0 flex-grow-0">
         <Logo />
       </div>
-    </Link>
+    </a>
   );
 };
 
