be995fd4985774f8b28251ec4c0b8f82ca42ad97
Huy Tuyen <hbn@vmi1824828.contaboserver.net>
2024-07-12 20:57:58 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 source .env
-git pull && \
-docker build ./fe -t chillax-fe:${FE_VERSION} && \
-docker rmi -f $(docker images -f "dangling=true" -q) \
-docker compose down && \
-docker compose up -d --remove-orphans
+OLD_VERSION=$FE_VERSION
+echo "> UNTAG OLD IMAGE $OLD_VERSION" &&
+
+    # Pull source code
+    echo "> PULL SOURCE CODE" &&
+    git pull &&
+    source .env &&
+    NEW_VERSION=$FE_VERSION &&
+    if [ "$NEW_VERSION" != "$OLD_VERSION" ]; then
+        sed -i~ s/^FE_VERSION.*/FE_VERSION=${NEW_VERSION}/ ./fe/.env.local
+        docker rmi chillax-fe:${FE_VERSION} -f &&
+            echo "> BUILD FE VERSION $FE_VERSION" &&
+            docker build ./fe -t chillax-fe:${FE_VERSION} &&
+            # Run container
+            echo "> RUN CONTAINER" &&
+            docker compose down &&
+            docker compose up -d --remove-orphans &&
+            # Remove unuse image
+            echo "> REMOVE UNUSE IMAGE" &&
+            runningImages=$(docker ps --format {{.Image}}) &&
+            docker rmi $(docker images --format {{.Repository}}:{{.Tag}} | grep -v "$runningImages")
+    else
+        echo "Unchange version $FE_VERSION"
+    fi
+
+rm ./fe/.env.local~
