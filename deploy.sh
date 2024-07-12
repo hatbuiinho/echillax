10db9dd5be5c0fb4a26ae8cb3e4f3806270c43e8
Huy Tuyen <hbn@vmi1824828.contaboserver.net>
2024-07-12 18:38:14 +0800

    update deployment

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source ./fe/.version
+git pull && \
+docker build ./fe -t chillax-fe:${FE_VERSION} && \
+docker rmi -f $(docker images -f "dangling=true" -q) \
+docker compose down && \
+docker compose up -d --remove-orphans
