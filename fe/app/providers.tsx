fca7d64598f8a9998c613f37b1f93ca55fc626c3
Huy Tuyen <fsoft.datnn@gmail.com>
2024-07-18 13:34:24 +0700

    update SEO, truncate lib

--- fe/app/providers.tsx
+++ fe/app/providers.tsx
@@ -5,8 +5,6 @@ import { NextUIProvider } from "@nextui-org/system";
 import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
-import { PrimeReactProvider } from "primereact/api";
-import Tailwind from "primereact/passthrough/tailwind";
 
 export interface ProvidersProps {
   children: React.ReactNode;
@@ -17,10 +15,8 @@ export function Providers({ children, themeProps }: ProvidersProps) {
   const router = useRouter();
 
   return (
-    <PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}>
-      <NextUIProvider navigate={router.push}>
-        <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
-      </NextUIProvider>
-    </PrimeReactProvider>
+    <NextUIProvider navigate={router.push}>
+      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+    </NextUIProvider>
   );
 }
